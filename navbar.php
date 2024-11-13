dd7d1a93aaff3525aa7fb977a08ea61479dd02c9
putrichantikaa <putrichantiikaa@gmail.com>
2024-11-13 15:26:27 +0700

    memperbaiki navbar dan index jurusan

--- navbar.php
+++ navbar.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg bg-success bg-info-subtle">
+<nav class="navbar navbar-expand-lg bg-success bg-danger-subtle">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">SIM</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -7,13 +7,13 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Beranda</a>
+          <a class="nav-link active" aria-current="page" href="/pece_is62/PROJECT/index.php">Beranda</a>
         </li>
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="#">Data Mahasiswa</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Data Jurusan</a>
+          <a class="nav-link active" aria-current="page" href="/pece_is62/PROJECT/modul-jurusan/index.php">Data Jurusan</a>
         </li>
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="#">Data Dosen</a>
