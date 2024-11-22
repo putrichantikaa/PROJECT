73aba2f964fc573aa4d2a3d28aa51944ba9b0f24
putrichantikaa <putrichantiikaa@gmail.com>
2024-11-22 15:06:50 +0700

    form edit

--- modul-jurusan/edit.php
+++ modul-jurusan/edit.php
@@ -1,9 +1,26 @@
+<?php
+#1. koneksi ke database
+include("../koneksi.php");
+
+#2. ambil id yang akan disunting
+$id = $_GET['id'];
+
+#3 menggambil semua record data berdasaarkan id yang dipilih
+$ambil = "SELECT * FROM jurusans WHERE id = '$id'";
+
+#4. menjalakan query
+$edit = mysqli_query($koneksi,$ambil);
+
+#5. memisahkan record data  erdasarkan field/kolom
+$data = mysqli_fetch_array($edit);
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Data Jurusan</title>
+    <title>Edit Jurusan</title>
     <link rel="stylesheet" href="../css/bootstrap.css">
 </head>
 <body>
@@ -16,20 +33,21 @@
         <div class="col-8 m-auto">
             <div class="card">
             <div class="card-header">
-            <h3 class="float-start"> Form Data Jurusan</h3>
+                <h3 class="float-start">Form Edit Data Jurusan</h3>
                 
             </div>
     <div class="card-body">
-    <form action="proses.php" method="POST" enctype="multipart/form-data">
+    <form action="update.php" method="POST">
+        <input type="hidden" name="id" value="<?=$data['id'] ?>">
   <div class="mb-3">
     <label for="exampleInputEmail1" class="form-label">Kode Jurusan</label>
-    <input type="text" name="kode" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <input type="text" readonly value="<?=$data['kode'] ?>" name="kode" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
   </div>
   <div class="mb-3">
     <label for="exampleInputPassword1" class="form-label">Nama Jurusan</label>
-    <input type="text" name="jurusan" class="form-control" id="exampleInputPassword1">
+    <input type="text" value="<?=$data['jurusan'] ?>" name="jurusan" class="form-control" id="exampleInputPassword1">
   </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
+  <button type="submit" class="btn btn-primary">Update</button>
 </form>
                
             </div>
